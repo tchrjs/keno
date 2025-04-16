35fbdcd4ec572bfb4cd0dabff16dd3b9646f0b5e
Christian Toribio <chris09toribio@gmail.com>
2025-04-16 14:36:17 -0700

    removed unnecessary exports

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,8 +1,6 @@
 class_name Game extends Node
 
 @export var state_machine: StateMachine
-@export var board: Board
-@export var payouts: Payouts
 
 # Connect signals to children, then start up game.
 func _ready() -> void:
