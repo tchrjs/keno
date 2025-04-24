53a4a4f2e3677e11e5e65ce819c194b8b6a8b984
Christian Toribio <chris09toribio@gmail.com>
2025-04-23 21:17:30 -0700

    added game pause

--- scenes/game/game.gd
+++ scenes/game/game.gd
@@ -1,21 +1,9 @@
 class_name Game extends Node
 
-@export var state_machine: StateMachine
+@export var game_content: GameContent
+@export var pause_menu: PauseMenu
 
-@export var board: Board
-@export var payouts: Payouts
-@export var console: Console
-@export var info_bar: InfoBar
-
-# Connect signals to children, then start up game.
-func _ready() -> void:
-	board.connect("marked_spots_updated", reset)
-	console.bet_meter.connect("bet_updated", reset)
-	console.play_button.connect("play_pressed", reset)
-	state_machine.enter_initial_state()
-
-func reset() -> void:
-	board.reset()
-	payouts.reset()
-	payouts.update()
-	info_bar.update()
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("game_pause"):
+		pause_menu.visible = !pause_menu.visible
+		get_tree().paused = pause_menu.visible
